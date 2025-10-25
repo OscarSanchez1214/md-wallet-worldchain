1dc839341ac9843af1316d1d1e575fffee4ea690
Oscar Sanchez <78560380+OscarSanchez1214@users.noreply.github.com>
2025-10-25 18:09:21 -0500

    Update QRScanner.tsx

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -1,28 +1,26 @@
 "use client";
-
-import { QrReader } from "react-qr-reader";
+import QrReader from "react-weblineindia-qr-reader";
 
 export const QRScanner = ({ onScan }: { onScan: (address: string) => void }) => {
-  const handleScan = (result: any) => {
-    if (result?.text) {
-      onScan(result.text);
+  const handleScan = (data: string | null) => {
+    if (data) {
+      onScan(data);
     }
   };
 
-  const handleError = (error: any) => {
-    console.error("Error al escanear QR:", error);
+  const handleError = (err: any) => {
+    console.error("Error al escanear QR:", err);
   };
 
   return (
     <div className="mt-4">
       <h3 className="font-semibold mb-2">Escanear QR</h3>
       <QrReader
-        onResult={(result, error) => {
-          if (!!result) handleScan(result);
-          if (!!error) handleError(error);
-        }}
-        constraints={{ facingMode: "environment" }}
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
         style={{ width: "100%" }}
+        facingMode="environment"
       />
     </div>
   );
