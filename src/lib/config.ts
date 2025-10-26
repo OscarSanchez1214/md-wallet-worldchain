57d89e9532fe7162dea3e283cd0ac7b8822930c3
Oscar Sanchez <78560380+OscarSanchez1214@users.noreply.github.com>
2025-10-25 19:38:44 -0500

    Create config.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,2 @@
+export const MD_TOKEN = "0x6335c1F2967A85e98cCc89dA0c87e672715284dB";
+export const RPC_URL = import.meta.env.VITE_RPC_URL || "https://worldscan.org/token/0x6335c1F2967A85e98cCc89dA0c87e672715284dB";
